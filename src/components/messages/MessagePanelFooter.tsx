07b5133891bd2f39f0850a633e27c99ac89b4787
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-08-05 12:56:29 +0700

    add message input field functionality & call api to create message

--- src/components/messages/MessagePanelFooter.tsx
+++ src/components/messages/MessagePanelFooter.tsx
@@ -1,12 +1,61 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { postNewMessage } from "@/utils/api";
+import { useParams } from "react-router-dom";
+
+const messageFormSchema = z.object({
+  content: z.string().min(1),
+});
 
 const MessagePanelFooter = () => {
+  const { id } = useParams();
+  const form = useForm<z.infer<typeof messageFormSchema>>({
+    resolver: zodResolver(messageFormSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof messageFormSchema>) => {
+    console.log("🚀 ~ onSubmit ~ values:", values);
+    if (!id) return;
+    try {
+      await postNewMessage({
+        conversationId: parseInt(id),
+        content: values.content,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      form.reset();
+    }
+  };
+
   return (
     <div className='flex mx-3'>
-      <Input
-        placeholder='Aa'
-        className='border-none focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-neutral-400 rounded-3xl text-neutral-100 bg-neutral-700'
-      />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
+          <FormField
+            control={form.control}
+            name='content'
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder='Aa'
+                    className='w-full border-none focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-neutral-400 rounded-3xl text-neutral-100 bg-neutral-700'
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 };
