b40c5d7e44b18cf0462f32f94b39c7086103c86f
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-08-07 22:28:08 +0700

    add withCredentials for socket io connection

--- src/utils/constants/socket.ts
+++ src/utils/constants/socket.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_REACT_APP_WEBSOCKET_URL);
+const socket = io(import.meta.env.VITE_REACT_APP_WEBSOCKET_URL, {
+  withCredentials: true,
+});
 
 export default socket;
