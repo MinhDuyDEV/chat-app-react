4399b246e40a4c514d87c81e5bba7c0899f6976d
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-08-08 22:39:43 +0700

    refactor code

--- src/utils/constants/socket.ts
+++ src/utils/constants/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_REACT_APP_WEBSOCKET_URL, {
-  withCredentials: true,
+  withCredentials: false,
 });
 
 export default socket;
